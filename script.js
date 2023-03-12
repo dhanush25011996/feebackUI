5bc3143f5a8300cad8a0ed03651195e09d4549c6
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2023-03-12 22:27:52 +0530

    Added different feedback messages using switch case

--- script.js
+++ script.js
@@ -7,27 +7,40 @@ const containerEl = document.getElementById("container");
 let selectedRating = "";
 
 ratingEls.forEach((ratingEl) => {
-    ratingEl.addEventListener("click",(event) => {
-        removeActive();
-        selectedRating = event.target.innerText || event.target.parentNode.innerText;
-        event.target.classList.add("active");
-        event.target.parentNode.classList.add("active");
-    });
+  ratingEl.addEventListener("click",(event) => {
+    removeActive();
+    selectedRating = event.target.innerText || event.target.parentNode.innerText;
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
+  });
 });
+
 btnEl.addEventListener("click", () => {
-    if(selectedRating !== ""){
-        containerEl.innerHTML = `
-        <strong>Thank You!</strong>
-        <br>
-        <br>
-        <strong>Feedback: ${selectedRating}</strong>
-        <p>We will use your feedback to improve our customer support</p>
-        `;
+  if(selectedRating !== ""){
+    let feedbackMessage = "";
+    switch(selectedRating) {
+      case "Unhappy":
+        feedbackMessage = "We're sorry to hear that you weren't satisfied with our service. Please let us know how we can improve.";
+        break;
+      case "Neutral":
+        feedbackMessage = "Thanks for your feedback! We're always looking for ways to improve our service, so please let us know if you have any suggestions.";
+        break;
+      case "Happy":
+        feedbackMessage = "Thanks for letting us know that you had a positive experience! We're glad that we could help.";
+        break;
     }
-})
+    containerEl.innerHTML = `
+      <strong>Thank You!</strong>
+      <br>
+      <br>
+      <strong>Feedback: ${selectedRating}</strong>
+      <p> ${feedbackMessage} </p>
+    `;
+  }
+});
 
 function removeActive(){
-    ratingEls.forEach((ratingEl) => {
-        ratingEl.classList.remove("active");
-    });
+  ratingEls.forEach((ratingEl) => {
+    ratingEl.classList.remove("active");
+  });
 }
